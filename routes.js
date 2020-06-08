73d02016df56cd611c7b5dbe4dcf38494b5df477
linhle <linhle@Hieus-MacBook-Air.local>
2020-06-08 18:04:50 +0800

    ad about page

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ const productRepository = require('./repositories/productRepository');
 module.exports = (app) => {
     app.get('/', (req, res) => res.redirect('/products')); 
     app.get('/login', productController.login ); //log in to be able to  edit/delte/create product
+    app.get('/about', (req, res) => res.render('about.ejs'));
     app.get('/logout', adminController.logOut); //log out
     app.get('/products', productController.getAll); // show all products
     app.get('/cart', userController.cart); // see user's cart
